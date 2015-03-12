acae2cb04a7747785bf0db7f8650ca1126adc2ad
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-03-11 22:31:02 -0400

    Reconfigured routing

---
+++ PersonalWebsite/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class BlogController : Controller
+    {
+      
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+       
+    }
+}
--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -13,37 +13,37 @@ namespace PersonalWebsite.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Projects()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Projects()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Blog()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Blog()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Resume() 
-        {
-            return View();
-        }
+        //public ActionResult Resume() 
+        //{
+        //    return View();
+        //}
     }
 }
