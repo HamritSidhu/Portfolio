fae520ec8fd5d50d0889c18f07ed2e8497dc4a79
HamritSidhu <hamritsidhu_94@hotmail.com>
2015-04-01 23:48:55 -0400

    about and blog page welcome messages

--- PersonalWebsite/Scripts/Custom/about.js
+++ PersonalWebsite/Scripts/Custom/about.js
@@ -1,6 +1,14 @@
 ﻿var about = {
     init: function () {
+        overview = document.getElementById("about-overview");
+
+        this.generateKeywordCircles();
+    },
+
+    generateKeywordCircles: function () {
+      
     }
+
 }
 
 about.init();
